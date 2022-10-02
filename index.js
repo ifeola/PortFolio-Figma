336be78ff2553a1ea0359ee47c61230137a3e6a3
DS Ifeola <nomorelimit11@gmail.com>
2022-10-02 10:37:15 +0100

    additional commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Toggle tools
+const linkDesign = document.querySelector("#one");
+const linkWeb = document.querySelector("#two");
+const web = document.querySelector("#web");
+const design = document.querySelector("#design");
+
+linkDesign.addEventListener("click", function () {
+  design.style.transform = "translate(0)";
+  web.style.transform = "translate(200%)";
+  design.style.transitionDelay = "0.4s";
+  web.style.transitionDelay = "0s";
+});
+
+linkWeb.addEventListener("click", function () {
+  web.style.transform = "translate(0)";
+  design.style.transform = "translate(200%)";
+  web.style.transitionDelay = "0.4s";
+  design.style.transitionDelay = "0s";
+});
+
+const trigger = document.querySelectorAll(".btn");
+trigger.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle() {
+  trigger.forEach((item) =>
+    item != this ? item.classList.remove("is-active") : null
+  );
+
+  if (this.classList != "is-active") {
+    this.classList.toggle("is-active");
+  }
+}
